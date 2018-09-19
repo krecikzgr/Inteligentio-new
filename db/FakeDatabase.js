b57b18f94edbd2faa12ff515616b6cbf8ce92717
Adrian <adrian@Mac-mini-Adrian.local>
2018-09-19 22:59:17 +0200

    WIP

--- db/FakeDatabase.js
+++ db/FakeDatabase.js
@@ -8,9 +8,37 @@ const IN_MEMORY_SENSORS = [
     }
 ];
 
+const IN_MEMORY_USERS = [
+    {
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+          },
+          username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+          },
+          password: {
+            type: String,
+            required: true,
+          },
+          passwordConf: {
+            type: String,
+            required: true,
+          }
+    }
+]
+
 
 module.exports = {
     getSwitchesTable() {
         return IN_MEMORY_SENSORS;
+    },
+    getUsersTable() {
+        return IN_MEMORY_USER;
     }
 };
